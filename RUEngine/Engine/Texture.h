ecfc0f4c44258173e132ff447e142b6d4b1065e8
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-22 15:15:11 +0100

    Update documentation

--- RUEngine/Engine/Texture.h
+++ RUEngine/Engine/Texture.h
@@ -9,9 +9,14 @@
 
 namespace tex {
           /**
-              *  Calls the SOIL2 and gl functions.
+              * Calls the SOIL2 and gl functions
+              * Takes in the texture path
               */
           GLuint loadTexture(const char* path);
+          /**
+              * Calls the SOIL2 and gl functions
+              * Takes in a vector which has 6 different paths for the cubes faces
+              */
           GLuint loadCubemap(std::vector<const char*> faces);
 }
 #endif // TEXTURE_H
