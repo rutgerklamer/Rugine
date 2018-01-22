9849111fb96501a4e2db82e3bcad07075912e32c
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-22 11:55:25 +0100

    Update documentation

--- RUEngine/Engine/SceneManager.h
+++ RUEngine/Engine/SceneManager.h
@@ -13,19 +13,28 @@ class SceneManager
 {
     public:
       /**
-          *  Constructor
+          * Constructor
           */
         SceneManager(Input* input, ResourceManager* rm, Shader* s);
         /**
-            *  Destructor
+            * Destructor
             */
         virtual ~SceneManager();
         /**
-            *  //TODO Use this or remove it!
+            * Switch to next scene in the scenes list
             */
         void nextScene();
+        /**
+            * Switch to previous scene in the scenes list
+            */
         void prevScene();
+        /**
+            * Delete the current scene
+            */
         void destroyScene();
+         /**
+            * Update loop called to check what the state of the current scene is
+            */
         void checkState();
         std::vector<Superscene*> scenes;
         int currentscene;
