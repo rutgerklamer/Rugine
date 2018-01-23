79662980efb1b758d7d08a7f8f60f928a1d4c6d0
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-23 09:07:09 +0100

    Update documentation

--- RUEngine/MB/Bullet.h
+++ RUEngine/MB/Bullet.h
@@ -7,21 +7,43 @@
 class Bullet : public Entity
 {
 public:
+    /**
+        * This is used to determine where the bullet came from,
+        * Bullets collide with enemies and the players,
+        * And I didn't want to keep track of two vectors
+        */
     enum Origin {
       ENEMY, PLAYER
     };
+    /**
+        * Constructor
+        * Takes in glm::vec3 direction, this is the direction the bullet will fly
+        * Takes in a Origin o, This is where the bullet originated from
+        */
     Bullet(glm::vec3 direction, Origin o);
+    /**
+        * Destructor
+        */
     ~Bullet();
-
+    /**
+        * Update loop, part of the Rugine
+        * Takes in float deltaTime, You probably want to use this
+        */
     virtual void update(float deltaTime);
+    /** 
+        * This will reflect the bullet in the X direction,
+        * Used when it's colliding with a mirror
+        */
     void mirrorDirX();
+    /** 
+        * This will reflect the bullet in the Z direction,
+        * Used when it's colliding with a mirror
+        */
     void mirrorDirZ();
     Origin origin;
     Time time;
 private:
     glm::vec3 dir;
-
-
 };
 
 #endif
