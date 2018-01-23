d64cfdfa68690f63fadbb5ed071d1e97cf9b2c81
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-23 09:03:14 +0100

    Update documentation

--- RUEngine/Engine/Water.h
+++ RUEngine/Engine/Water.h
@@ -1,4 +1,3 @@
-
 #ifndef WATER_H
 #define WATER_H
 
@@ -9,7 +8,15 @@
 class Water
 {
 public:
+    /**
+        * Constructor
+        * Takes in glm::vec2 s, this is used to determine the size of the 2d water plane
+        * Takes in a glm::vec3 p, This is the position of the water plane
+        */
     Water(glm::vec2 s, glm::vec3 p);
+    /** 
+        * Destructor
+        */
     ~Water();
     glm::vec2 waterSize;
     glm::vec3 waterPosition;
