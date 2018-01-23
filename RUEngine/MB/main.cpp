d17bcadce63ab0856879d5ab3044f101799c2ca9
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-23 09:53:42 +0100

    Update documentation

--- RUEngine/MB/main.cpp
+++ RUEngine/MB/main.cpp
@@ -8,6 +8,7 @@
 #include "MB/Scene1.h"
 #include "MB/StartMenu.h"
 
+// Declare types
 Display* display;
 Scene* scene;
 Scene1* scene1;
@@ -15,14 +16,19 @@ StartMenu* startscene;
 
 int main()
 {
+  // Make a new Display, This will load all necessary framework related classes
   display = new Display();
+  // Make the scenes we have and give them the input display created for us
   startscene = new StartMenu(display->input);
   scene = new Scene(display->input);
   scene1 = new Scene1(display->input);
+  // Add those scenes to the scenemanager
   display->addScene(startscene);
   display->addScene(scene);
   display->addScene(scene1);
+  // Start the main gameloop
   display->gameLoop();
+  // If the gameloop stops we can delete everything we created here
   delete scene;
   delete scene1;
   delete startscene;
