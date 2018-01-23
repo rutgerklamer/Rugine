d29b8ed8f73dd6735e83432eac4f9717aa92a9ab
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-23 09:11:06 +0100

    Update documentation

--- RUEngine/MB/CameraEffects.h
+++ RUEngine/MB/CameraEffects.h
@@ -8,15 +8,44 @@
 class CameraEffects : public Entity
 {
 public:
+    /**
+        * Constructor
+        * Takes in a Camera* c, this is the camera we want to pull the effects on
+        * Takes in Entity* p, This is the player we need for checking
+        */
     CameraEffects(Camera* c, Entity* p);
+    /**
+        * Destructor
+        */
     ~CameraEffects();
-
+    /** 
+        * Update, part of Rugine
+        * Takes in float deltaTime, you probably want to use this
+        */
     virtual void update(float deltaTime);
+    /** 
+        * Pull of a transition to 3D
+        */
     void transition3D();
+    /** 
+        * Pull of a transition to 2D
+        */
     void transition2D();
+    /**
+        * Check if the level is loaded
+        */
     void levelLoaded();
+    /** 
+        * Check if the transaction is finished and we can load a new level
+        */
     bool loadLevel();
+    /** 
+        * Check if the transaction is finished and we can play
+        */
     bool getCanPlay();
+    /** 
+        * Get the height of the camera
+        */
     float getCamHeight();
 private:
   Camera* cam;
