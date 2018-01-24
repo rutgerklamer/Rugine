6ea409f08729e75fe526910e3ad1e923a68ee94b
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-24 09:58:44 +0100

    Update documentation

--- RUEngine/MB/Wall.h
+++ RUEngine/MB/Wall.h
@@ -6,9 +6,18 @@
 class Wall : public Entity
 {
 public:
+    /**
+        * Constructor
+        */
     Wall();
+    /**
+        * Destructor
+        */
     ~Wall();
-
+    /** 
+           * Update, part of Rugine
+           * Takes in float deltaTime, you probably want to use this
+           */
     virtual void update(float deltaTime);
 
 private:
