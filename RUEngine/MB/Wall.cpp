bbe4f280e3807c0fe3be9771fd8aa13bc7328f1c
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-24 10:23:05 +0100

    Update documentation

--- RUEngine/MB/Wall.cpp
+++ RUEngine/MB/Wall.cpp
@@ -3,8 +3,11 @@
 
 Wall::Wall() : Entity()
 {
+  // Load an OBJ file and set it as a mesh, we don't want it to have a normal map so we set it to false
   this->LoadObject("Assets/untitled.obj", false);
+  // Set the color
   this->setColor(glm::vec3(1,1,0));
+  // Make it reflective
   this->reflective = true;
 }
 
