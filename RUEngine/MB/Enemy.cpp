6b1ca5c6286658a6d9d77f311be7621c41886589
Rutger Klamer <rutger@rutgerklamer.nl>
2018-01-24 10:15:02 +0100

    Update documentation

--- RUEngine/MB/Enemy.cpp
+++ RUEngine/MB/Enemy.cpp
@@ -2,7 +2,9 @@
 
 Enemy::Enemy(Entity* entity) : Entity()
 {
+  // Start a timer so dont spawn a bullet every frame
   time.timer.start();
+  // Set the target (player)
   player = entity;
 }
 
@@ -13,12 +15,18 @@ Enemy::~Enemy()
 
 void Enemy::update(float deltaTime) {
   if (time.timer.seconds() > 0.1f) {
+    // Make a new bullet and set the direction + origin
     Bullet* bullet = new Bullet(glm::normalize(player->position - this->position) * 100.0f, Bullet::ENEMY);
+    // the bullet should spawn at the center of this entity
     bullet->position = this->position;
+    // Push the bullet to the list of bullets
     bullets.push_back(bullet);
+    // set the timer to 0 seconds again
     time.timer.start();
   }
+  // Get the normalizer difference between this entity and the player
   glm::vec3 v = glm::normalize(player->position - this->position);
+  // So we can give it the right rotation
   this->eularAngles = glm::vec3(0,-atan2(v.z, v.x),0);
 
 }
